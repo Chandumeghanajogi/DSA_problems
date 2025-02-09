99fa11c46f7d7b4f2b353b38ff982734c77a47ff
Chandumeghanajogi <jogiveera1234@gmail.com>
2025-02-09 10:59:59 +0530

    day-1 problem

---
+++ twosum.py
@@ -0,0 +1,48 @@
+#Date:09-02-2024
+#two sum
+# to find the sum of 2 numbers in the list to give the target and print the  indices of those numbers
+# example:
+# a=[2,7,11,15]
+# target=9
+
+# solutions :
+# 1.brute force
+# 2.better using the hashing/dictionary
+# 3.using the two pointer approach
+
+# solutions:
+# 1. brute force
+
+def twosum(a,target):
+    for i in range(len(a)):
+        for j in range(len(a)):
+            if i==j:
+                continue
+            else:
+                if a[i]+a[j]==target:
+                    return [i,j]
+
+a=list(map(int,input().split()))
+target=int(input())
+print(twosum(a,target))
+
+# time complexity:O(n^2)
+
+# 2. better using the hashing/dictionary
+
+def twoSum(nums,target):
+    dictionary={}
+    for i in range(len(nums)):
+        if nums[i] in dictionary:
+            return [dictionary[nums[i]],i]
+        else:
+            dictionary[target-nums[i]]=i
+
+nums=list(map(int,input().split()))
+target=int(input())
+print(twoSum(nums,target))
+
+# time complexity:O(n)
+
+
+
